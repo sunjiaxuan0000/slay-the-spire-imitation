987159a810b873b0433bc9124c330e997b2075ab
sjx <bdlfy_0902@qq.com>
2026-09-09 13:09:13 +0800

    处理合并冲突

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-module com.example.thegame {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-    requires com.almasb.fxgl.all;
-
-    opens com.example.thegame to javafx.fxml;
-    exports com.example.thegame;
-=======
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
@@ -16,5 +5,4 @@ module com.example.demo {
 
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
->>>>>>> hl-bog
 }
