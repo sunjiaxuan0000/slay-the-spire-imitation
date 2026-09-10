593ea60c6507f223ff8f530a722cee0d408d0bb7
wuliao <18995826780@163.com>
2026-09-10 09:23:20 +0800

    合并主枝

--- src/main/java/com/example/demo/Launcher.java
+++ src/main/java/com/example/demo/Launcher.java
@@ -1,8 +1,6 @@
-<<<<<<<< HEAD:src/main/java/com/example/thegame/day1/Launcher.java
-package com.example.thegame.day1;
-========
+
 package com.example.demo;
->>>>>>>> origin/sjx:src/main/java/com/example/demo/Launcher.java
+
 
 import javafx.application.Application;
 
